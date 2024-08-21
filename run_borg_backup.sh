f366789419f23b24d8df8d6df00db9679a0261e5
Andrey Goncharov <andrey@goncharov.page>
2024-08-21 11:43:58 +0300

    Add selective stack data backup

--- run_borg_backup.sh
+++ run_borg_backup.sh
@@ -16,13 +16,13 @@ run_borg () {
 run_borg "${ROOT_SYNCTHING_PATH}/Bills" "${ROOT_BACKUP_PATH}/bills"
 run_borg "${ROOT_SYNCTHING_PATH}/Books" "${ROOT_BACKUP_PATH}/books"
 run_borg "${HOME_PATH}/.config" "${ROOT_BACKUP_PATH}/home_config"
-run_borg "${STACKS_DATA_PATH}" "${ROOT_BACKUP_PATH}/stacks_data"
 run_borg "${STACKS_DEFINITIONS_ROOT}/config" "${ROOT_BACKUP_PATH}/stacks_config"
 run_borg "${STACKS_DEFINITIONS_ROOT}/definitions" "${ROOT_BACKUP_PATH}/stacks_definitions"
 run_borg "/mnt/media/mail_archives" "${ROOT_BACKUP_PATH}/mail_archives"
 run_borg "${STACKS_DATA_PATH}/offlineimap" "${ROOT_BACKUP_PATH}/mail_current"
 run_borg "${ROOT_SYNCTHING_PATH}/Obsidian" "${ROOT_BACKUP_PATH}/obsidian"
 run_borg "${ROOT_SYNCTHING_PATH}/Personal" "${ROOT_BACKUP_PATH}/personal"
+run_borg "${STACKS_DATA_PATH}/baikal" "${ROOT_BACKUP_PATH}/baikal"
 
 # Custom runs
 MINIFLUX_BACKUP_PATH="/mnt/media/backups/miniflux"
@@ -30,9 +30,6 @@ borg create --content-from-command "$MINIFLUX_BACKUP_PATH::{now}" -- docker exec
 borg prune -s --keep-daily 3 --keep-weekly=4 "$MINIFLUX_BACKUP_PATH"
 borg compact "$MINIFLUX_BACKUP_PATH"
 
-IMMICH_SOURCE_PATH="${STACKS_DATA_PATH}/immich"
-IMMICH_BACKUP_PATH="${ROOT_BACKUP_PATH}/immich"
-docker exec -t immich_postgres pg_dumpall --clean --if-exists --username=postgres > "${IMMICH_SOURCE_PATH}/database-backup/immich-database.sql"
 borg create -s "$IMMICH_BACKUP_PATH::{now}" "$IMMICH_SOURCE_PATH" --exclude "${IMMICH_SOURCE_PATH}/thumbs/" --exclude "${IMMICH_SOURCE_PATH}/encoded-video/"
 borg prune -s --keep-daily 3 --keep-weekly=4 "$IMMICH_BACKUP_PATH"
 borg compact "$IMMICH_BACKUP_PATH"
